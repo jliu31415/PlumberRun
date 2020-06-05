faf719c46cd8963af402b5ab373a97c01e5281e2
jliu31415 <jliu31415@gmail.com>
2020-06-05 14:50:03 -0400

    recyclerView implementation

--- app/src/main/java/jliu/plumberrun/MainActivity.java
+++ app/src/main/java/jliu/plumberrun/MainActivity.java
@@ -1,16 +1,21 @@
 package jliu.plumberrun;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> levelNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final AppCompatActivity main = this;
+        final AppCompatActivity context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -18,8 +23,24 @@ public class MainActivity extends AppCompatActivity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(new Game(main, 0));
+                setContentView(R.layout.recycler_view);
+                setLevelNames();
+                LinearLayoutManager layoutManager = new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+                RecyclerView recyclerView = findViewById(R.id.recyclerView);
+                recyclerView.setLayoutManager(layoutManager);
+                RecyclerViewAdapter adapter = new RecyclerViewAdapter(context, levelNames);
+                recyclerView.setAdapter(adapter);
+                //setContentView(new Game(context, 0));
             }
         });
+    }
+
+    private void setLevelNames() {
+        levelNames = new ArrayList<>();
+        levelNames.add("Level 1");
+        levelNames.add("Level 2");
+        levelNames.add("Level 3");
+        levelNames.add("Level 4");
+        levelNames.add("Level 5");
     }
 }
---
+++ app/src/main/java/jliu/plumberrun/RecyclerViewAdapter.java
@@ -0,0 +1,51 @@
+package jliu.plumberrun;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<String> levelNames;
+
+    RecyclerViewAdapter(Context context, ArrayList<String> levelNames) {
+        this.context = context;
+        this.levelNames = levelNames;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.level_select, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.levelName.setText(levelNames.get(position));
+//        Glide.with(context)
+//                .load(.get(position))
+//                .into(holder.);
+    }
+
+    @Override
+    public int getItemCount() {
+        return levelNames.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView levelName;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            levelName = itemView.findViewById(R.id.levelName);
+        }
+    }
+}
